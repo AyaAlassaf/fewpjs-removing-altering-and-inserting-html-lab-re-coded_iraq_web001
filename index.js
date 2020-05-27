3be7902afd5b981a4d3de273704a210473f7fc48
Aya Alassaf <ayafaisal242@yahoo.com>
2020-05-27 17:24:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Write your code here!
 let main=document.querySelector('main#main');
 main.remove();
-let newHeader=document.createelement('h1#victory');
+let newHeader=document.createElement('h1#victory');
 document.body.appendchild(newHeader)
 newHeader.innerHTML="is the champion";
     
